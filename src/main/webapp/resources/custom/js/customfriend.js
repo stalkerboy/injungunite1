b8ebfe830de3474b4f60d6366c7234f85d1d9d21
bit-dummy <bit-dummy@dummycom>
2016-04-20 15:28:47 +0900

    프렌드리스트 메세지보내기 자동 아이디생성

--- src/main/webapp/resources/custom/js/customfriend.js
+++ src/main/webapp/resources/custom/js/customfriend.js
@@ -43,8 +43,8 @@ $("#searchfriend-btn").on("click", function(){
            	 	else if(userfind[i].ismyfriend==0) {
            	 		divstr += "<a href=\"#\" onclick=\"addfriend(" +userfind[i].fri_mem_snum +")\"><i class=\"fa fa-user-plus\"></i>Friend Add</a>" ;
            	 	}
-           		divstr+= "<hr class=\"divider\"><a href=\"/message/writemessageform\"><i class=\"fa fa-envelope-o\"></i>Sent Message</a></li></ul></li>";
-            } 
+           		divstr+= "<hr class=\"divider\"><a href=\"/message/writemessageform?mem_id="+ userfind[i].fri_mem_id +"\"><i class=\"fa fa-envelope-o\"></i>Sent Message</a></li></ul></li>";
+            }  
 			var frienddiv = document.getElementById("friendlist-div");
 			frienddiv.innerHTML = divstr;
 		}
@@ -75,7 +75,7 @@ function deletefriend(fri_snum){
            	 	else if(userfind[i].ismyfriend==0) {
            	 		divstr += "<a href=\"#\" onclick=\"addfriend(" +userfind[i].fri_mem_snum +")\"><i class=\"fa fa-user-plus\"></i>Friend Add</a>" ;
            	 	}
-           		divstr+= "<hr class=\"divider\"><a href=\"/message/writemessageform\"><i class=\"fa fa-envelope-o\"></i>Sent Message</a></li></ul></li>";
+           		divstr+= "<hr class=\"divider\"><a href=\"/message/writemessageform?mem_id="+friendlist[i].fri_mem_id +"\"><i class=\"fa fa-envelope-o\"></i>Sent Message</a></li></ul></li>";
             } 
 			var frienddiv = document.getElementById("friendlist-div");
 			frienddiv.innerHTML = divstr;
@@ -107,7 +107,8 @@ function addfriend(mem_snum){
            	 	else if(userfind[i].ismyfriend==0) {
            	 		divstr += "<a href=\"#\" onclick=\"addfriend(" +userfind[i].fri_mem_snum +")\"><i class=\"fa fa-user-plus\"></i>Friend Add</a>" ;
            	 	}
-           		divstr+= "<hr class=\"divider\"><a href=\"/message/writemessageform\"><i class=\"fa fa-envelope-o\"></i>Sent Message</a></li></ul></li>";
+           		divstr+= "<hr class=\"divider\"><a href=\"/message/writemessageform?mem_id="+friendlist[i].fri_mem_id +"\"><i class=\"fa fa-envelope-o\"></i>Sent Message</a></li></ul></li>";
+           		
             } 
 			var frienddiv = document.getElementById("friendlist-div");
 			frienddiv.innerHTML = divstr;
@@ -139,7 +140,7 @@ function deletefriendFromMyList(fri_snum){
 			tablestr = "<div class=\"box-header with-border\">";
 			for(var i=0;i<friendlist.length;i++){ 
   				var friend = friendlist[i].friendNo;
-				divstr += "<li class=\"dropdown friend-menu\"><div class=\"pull-right\"><span title=\"delete\" class=\"del-badge badge bg-red\" style=\"margin:0; display:none;\" onclick=\"deletefriendFromMyList("+friendlist[i].fri_snum+");\"><i class=\"fa fa-user-times\"></i></span></div><img src=\'/displayFile?fileName="+friendlist[i].profile +"\' class=\"friend-img\" /></br><a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\" aria-expanded=\"false\" style=\"color:black\">"+friendlist[i].fri_mem_id+"</a><ul class =\"dropdown-menu\"><li class=\"header\"><a href=\"/board/boardlist?user="+ friendlist[i].fri_mem_id +"\"><i class=\"fa fa-user\"></i>Friend Page</a><hr class=\"divider\"><a href=\"#\" onclick=\"deletefriendFromMyList(" +friendlist[i].fri_snum +")\"><i class=\"fa fa-user-times\"></i>Friend delete</a><hr class=\"divider\"><a href=\"#\" ><i class=\"fa fa-envelope-o\"></i>Sent Message</a></li></ul></li></div>";
+				divstr += "<li class=\"dropdown friend-menu\"><div class=\"pull-right\"><span title=\"delete\" class=\"del-badge badge bg-red\" style=\"margin:0; display:none;\" onclick=\"deletefriendFromMyList("+friendlist[i].fri_snum+");\"><i class=\"fa fa-user-times\"></i></span></div><img src=\'/displayFile?fileName="+friendlist[i].profile +"\' class=\"friend-img\" /></br><a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\" aria-expanded=\"false\" style=\"color:black\">"+friendlist[i].fri_mem_id+"</a><ul class =\"dropdown-menu\"><li class=\"header\"><a href=\"/board/boardlist?user="+ friendlist[i].fri_mem_id +"\"><i class=\"fa fa-user\"></i>Friend Page</a><hr class=\"divider\"><a href=\"#\" onclick=\"deletefriendFromMyList(" +friendlist[i].fri_snum +")\"><i class=\"fa fa-user-times\"></i>Friend delete</a><hr class=\"divider\"><a href=\"/message/writemessageform?mem_id="+friendlist[i].fri_mem_id +"\" ><i class=\"fa fa-envelope-o\"></i>Sent Message</a></li></ul></li></div>";
 			} 
  			var frienddiv = document.getElementById("friendlist-div");
  			frienddiv.innerHTML = divstr;
